6b5c680286c2e8024adc097a273e709febf47d50
spades2 <abdullahzahid9013@gmail.com>
2023-06-21 10:30:31 +0500

    commit 3

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const upload = multer({ storage: storage })
 
 
 const app=express();
-const port= 27017;
+const port= 3000;
 const hostname = '127.0.0.1';
 //EXPRESS RELATED
 app.use('/static' ,express.static('static'));
@@ -30,7 +30,7 @@ app.use(express.static(__dirname));
 app.use(express.urlencoded());
 // Set the view engine
 
-const connectionString = 'mongodb://localhost:27017/';
+const connectionString = 'mongodb+srv://AbdullahZahid:abdullah@mongoshongo.nrpswwo.mongodb.net/';
 mongoose.connect(connectionString, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
